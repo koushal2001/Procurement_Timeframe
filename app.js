3a29ea1f122071ea63eb2f4c7d83b55175016355
Koushal Bhat <60033544+koushal2001@users.noreply.github.com>
2023-02-09 13:09:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express();
 const bodyParser = require('body-parser');
 const { spawn } = require("child_process");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 
-app.get("/model",(req,res)=>{
+app.post("/model",(req,res)=>{
 
-    EST_COST_RANGE = req.body.EST_COST_RANGE;
+    EST_COST = req.body.EST_COST;
     MODEOFTENDER = req.body.MODEOFTENDER;
     METHOD_OF_PURCHASE = req.body.METHOD_OF_PURCHASE;
     FINANCIAL_POWER_CODE = req.body.FINANCIAL_POWER_CODE;
@@ -15,10 +15,11 @@ app.get("/model",(req,res)=>{
     CONCURRENCE_BY = req.body.CONCURRENCE_BY;
     BUDGET_HEAD_CODE = req.body.BUDGET_HEAD_CODE;
     IS_PAC = req.body.IS_PAC;
-    input_data = [EST_COST_RANGE ,MODEOFTENDER , METHOD_OF_PURCHASE,FINANCIAL_POWER_CODE, CFA_CODE,CONCURRENCE_BY, BUDGET_HEAD_CODE , IS_PAC]
-    const python = spawn("python", ["model.py", input_data ]);
+    input_data = [EST_COST ,MODEOFTENDER , METHOD_OF_PURCHASE,FINANCIAL_POWER_CODE, CFA_CODE,CONCURRENCE_BY, BUDGET_HEAD_CODE , IS_PAC]
+   
+    const python = spawn("python3", ["./input.py", input_data ],{ shell: true });
     
-
+    var processed_data;
     python.stdout.on("data", function (data) {
         processed_data = data.toString();
     });
@@ -34,7 +35,7 @@ app.get("/model",(req,res)=>{
     
 })
 
-app.listen(3000, () => {
-    console.log(`server is running on port 3000`);
+app.listen(4000, () => {
+    console.log(`server is running on port 4000`);
 })
 // module.exports = app;
